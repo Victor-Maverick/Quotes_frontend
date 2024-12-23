a41b54c39cc14b9dd8ad141233cb115849bfa5ae
Biokes <106444765+Biokes@users.noreply.github.com>
2024-12-23 22:14:51 +0100

    update all components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import './App.css';
+import QuoteDetails from './components/quote/QuoteDetails'; 
+import Homepage from './components/quote/Homepage';
+
+function App() {
+  return (
+    <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/quote-details" element={<QuoteDetails />} />
+          </Routes>
+        </div>
+      
+    </Router>
+  );
+}
+
+export default App;
+
